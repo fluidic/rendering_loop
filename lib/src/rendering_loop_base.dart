dd2f0a0e31e9b76f865b56d91cd0769d6166d1ea
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-03-02 14:26:03 +0900

    Add rendering_loop.
    rendering_loop library is extracted from seed.

--- lib/src/rendering_loop_base.dart
+++ lib/src/rendering_loop_base.dart
@@ -1,11 +1,121 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+library rendering_loop;
 
-// TODO: Put public facing types in this file.
+import 'dart:async';
+import 'dart:html';
 
-library rendering_loop.base;
+/// Phases in rendering of a frame. Rendering is proceeded to phases
+/// in the order listed in this `enum`.
+enum Phase {
+  /// DOM modifications should be performed in this phase in a frame.
+  update,
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+  /// DOM read-access that may cause reflow need to be performed in this phase
+  /// in a frame.
+  read,
+
+  /// Tasks that need to be performed after all DOM accesses should be performed
+  /// in this phase.
+  wrapUp
 }
+
+/// [Loop] rendering frames over and over again.
+class RenderingLoop {
+  /// The number of phases defined in [Phase].
+  static final int _phaseCount = Phase.values.length;
+
+  /// [Zone] used to execute a frame rendering.
+  Zone _renderingZone;
+
+  /// The current [Phase] of this [RenderingLoop].
+  Phase _currentPhase = null;
+
+  /// [List] of microtasks pending execution.
+  final List<Function> _microtasks = [];
+
+  /// Designates if there is a requested animation frame pending.
+  bool _isFramePending = false;
+
+  /// [List] of [Completer]s notifying the entering into the corresponding
+  /// [Phase]s.
+  List<Completer<Null>> _phaseCompleters = new List(_phaseCount);
+
+  /// [List] of [StreamController]s for each of [Phase]. Different
+  /// from [_phaseCompleters], [_phaseControllers] are created
+  /// only once.
+  List<StreamController<Null>> _phaseControllers = new List.generate(
+      _phaseCount, (_) => new StreamController.broadcast(sync: true));
+
+  RenderingLoop() {
+    _renderingZone = Zone.current.fork(
+        specification:
+        new ZoneSpecification(scheduleMicrotask: _scheduleMicrotask));
+  }
+
+  /// Returns whether a frame is being rendered or not.
+  bool get isRendering => _currentPhase != null;
+
+  /// Requests the given [phase] and returns a [Future] completed
+  /// when the given phase begins. The returned [Future] is a one-shot,
+  /// so when the [phase] is entered again, the returned [Future] won't
+  /// be completed.
+  Future<Null> enter(Phase phase) {
+    if (_currentPhase == null || phase.index < _currentPhase.index) {
+      // If the requested phase is already passed in this frame,
+      // requests another frame.
+      _requestFrame();
+    }
+    if (_phaseCompleters[phase.index] == null) {
+      final completer =
+      _phaseCompleters[phase.index] = new Completer<Null>.sync();
+      _phaseControllers[phase.index].addStream(completer.future.asStream());
+    }
+    return _phaseCompleters[phase.index].future;
+  }
+
+  /// Returns a [Stream] being notified when the given [phase] enters.
+  Stream<Null> onPhase(Phase phase) => _phaseControllers[phase.index].stream;
+
+  /// Handles rendering of a frame.
+  void _frameHandler(num time) {
+    _renderingZone.run(() {
+      _isFramePending = false;
+      for (var phaseId = 0; phaseId < _phaseCount; phaseId++) {
+        _currentPhase = Phase.values[phaseId];
+        while (_phaseCompleters[phaseId] != null) {
+          var completer = _phaseCompleters[phaseId];
+          _phaseCompleters[phaseId] = null;
+          completer.complete();
+          _runMicrotasks();
+        }
+      }
+      _currentPhase = null;
+    });
+  }
+
+  /// Requests an animation frame.
+  void _requestFrame() {
+    if (!_isFramePending) {
+      _isFramePending = true;
+      window.requestAnimationFrame(_frameHandler);
+    }
+  }
+
+  /// Runs all microtasks queued during the curent frame.
+  void _runMicrotasks() {
+    for (var task in _microtasks) {
+      task();
+    }
+    _microtasks.clear();
+  }
+
+  /// Schedules the microtasks issued in a frame being rendered.
+  void _scheduleMicrotask(Zone self, ZoneDelegate parent, Zone zone, task()) {
+    if (_currentPhase != null) {
+      _microtasks.add(task);
+    } else {
+      parent.scheduleMicrotask(zone, task);
+    }
+  }
+}
+
+final RenderingLoop renderingLoop = new RenderingLoop();
--- test/rendering_loop_test.dart
+++ test/rendering_loop_test.dart
@@ -1,21 +1,207 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
-library rendering_loop.test;
+@TestOn('browser')
+import 'dart:async';
 
 import 'package:rendering_loop/rendering_loop.dart';
 import 'package:test/test.dart';
 
-void main() {
-  group('A group of tests', () {
-    Awesome awesome;
+main() {
+  group('RenderingLoop', () {
+    RenderingLoop loop;
 
     setUp(() {
-      awesome = new Awesome();
+      loop = new RenderingLoop();
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('run single task and check if they proceed in phases', () async {
+      final result = [];
+
+      task() async {
+        result.add('null');
+        await loop.enter(Phase.update);
+        result.add('update');
+        await loop.enter(Phase.read);
+        result.add('read');
+        await loop.enter(Phase.wrapUp);
+        result.add('wrapUp');
+      }
+
+      await task();
+
+      expect(result, ['null', 'update', 'read', 'wrapUp']);
+    });
+
+    test('run two tasks and check if they proceed in phases', () async {
+      final result = [];
+
+      task1() async {
+        result.add('1: null');
+        await loop.enter(Phase.update);
+        result.add('1: update');
+        await loop.enter(Phase.read);
+        result.add('1: read');
+      }
+
+      task2() async {
+        result.add('2: null');
+        await loop.enter(Phase.read);
+        result.add('2: read');
+        await loop.enter(Phase.wrapUp);
+        result.add('2: wrapUp');
+      }
+
+      await Future.wait([task1(), task2()]);
+
+      expect(result, [
+        '1: null',
+        '2: null',
+        '1: update',
+        '2: read',
+        '1: read',
+        '2: wrapUp'
+      ]);
+    });
+
+    test('run three tasks and check if they proceed in phases', () async {
+      final result = [];
+
+      task1() async {
+        result.add('1: null');
+        await loop.enter(Phase.update);
+        result.add('1: update');
+        await loop.enter(Phase.read);
+        result.add('1: read');
+      }
+
+      task2() async {
+        result.add('2: null');
+        await loop.enter(Phase.read);
+        result.add('2: read');
+        await loop.enter(Phase.wrapUp);
+        result.add('2: wrapUp');
+      }
+
+      task3() async {
+        result.add('3: null');
+        await loop.enter(Phase.update);
+        result.add('3: update');
+        await loop.enter(Phase.read);
+        result.add('3: read');
+        await loop.enter(Phase.wrapUp);
+        result.add('3: wrapUp');
+      }
+
+      await Future.wait([task1(), task2(), task3()]);
+
+      expect(result, [
+        '1: null',
+        '2: null',
+        '3: null',
+        '1: update',
+        '3: update',
+        '2: read',
+        '1: read',
+        '3: read',
+        '2: wrapUp',
+        '3: wrapUp'
+      ]);
+    });
+
+    test('async function in a phase should work in the phase', () async {
+      final result = [];
+
+      task1() async {
+        result.add('1: null');
+        await loop.enter(Phase.update);
+        result.add('1: update');
+        await loop.enter(Phase.read);
+        result.add('1: read');
+      }
+
+      asyncFunc() async {
+        result.add('2: read - async function');
+      }
+
+      task2() async {
+        result.add('2: null');
+        await loop.enter(Phase.read);
+        result.add('2: read');
+        await asyncFunc();
+        await loop.enter(Phase.wrapUp);
+        result.add('2: wrapUp');
+      }
+
+      await Future.wait([task1(), task2()]);
+
+      expect(result, [
+        '1: null',
+        '2: null',
+        '1: update',
+        '2: read',
+        '1: read',
+        '2: read - async function',
+        '2: wrapUp'
+      ]);
+    });
+
+    test('should process a phase already passed in the next frame', () async {
+      final result = [];
+
+      task1() async {
+        result.add('1: null');
+        await loop.enter(Phase.update);
+        result.add('1: update');
+        await loop.enter(Phase.read);
+        result.add('1: read');
+      }
+
+      task2() async {
+        result.add('2: null');
+        await loop.enter(Phase.read);
+        result.add('2: read');
+        await loop.enter(Phase.update);
+        result.add('2: update');
+      }
+
+      await Future.wait([task1(), task2()]);
+
+      expect(result, [
+        '1: null',
+        '2: null',
+        '1: update',
+        '2: read',
+        '1: read',
+        '2: update'
+      ]);
+    });
+
+    test('should notify phase stream only once per entrance', () async {
+      final result = [];
+
+      loop.enter(Phase.update);
+      loop.enter(Phase.update);
+      loop.enter(Phase.update);
+
+      loop.onPhase(Phase.update).listen((_) => result.add('notified'));
+      await loop.enter(Phase.wrapUp);
+
+      expect(result, hasLength(1));
+    });
+
+    test('should notify phase stream every time a phase enters', () async {
+      final result = [];
+
+      loop.onPhase(Phase.update).listen((_) => result.add('notified'));
+
+      await loop.enter(Phase.update);
+      await loop.enter(Phase.wrapUp);
+
+      await loop.enter(Phase.update);
+      await loop.enter(Phase.wrapUp);
+
+      await loop.enter(Phase.update);
+      await loop.enter(Phase.wrapUp);
+
+      expect(result, hasLength(3));
     });
   });
 }
